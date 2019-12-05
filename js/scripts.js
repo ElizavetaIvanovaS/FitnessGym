2666e3fb86751e0eb802d13094ea2082b882293d
ElizavetaIvanovaS <57837419+ElizavetaIvanovaS@users.noreply.github.com>
2019-12-05 14:00:27 +0200

    End without responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,33 @@
 $(".menu-icon").on("click", function() {
   $(".navigation-mobile ul").toggle();
 });
+
+(function($) {
+  $(function() {
+    $("ul.tabs__caption").on("click", "li:not(.active)", function() {
+      $(this)
+        .addClass("active")
+        .siblings()
+        .removeClass("active")
+        .closest("div.tabs")
+        .find("div.tabs__content")
+        .removeClass("active")
+        .eq($(this).index())
+        .addClass("active");
+    });
+  });
+})(jQuery);
+
+jQuery(document).ready(function() {
+  $("ul.tabs__caption").on("click", "li:not(.active)", function() {
+    $(this)
+      .addClass("active")
+      .siblings()
+      .removeClass("active")
+      .closest("div.tabs")
+      .find("div.tabs__content")
+      .removeClass("active")
+      .eq($(this).index())
+      .addClass("active");
+  });
+});
